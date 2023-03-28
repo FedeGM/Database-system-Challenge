b488b4c0f8aafc869bb812251bdda309f419ade1
FedeGM <gomezmonroyfederico@gmail.com>
2023-03-27 22:50:25 -0300

    Get files from Blob Storage Done

--- shared_code/database.py
+++ shared_code/database.py
@@ -2,8 +2,8 @@ from sqlalchemy import create_engine, MetaData, Column, Integer, String, Table
 from sqlalchemy.orm import scoped_session, sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 
-from .database_connection import SQLconnection as SQL
-from .config import config_data
+from database_connection import SQLconnection as SQL
+from config import config_data
 
 azure_db = SQL(server= config_data['SERVER'],
                 database= config_data['DATABASE'],
--- shared_code/models.py
+++ shared_code/models.py
@@ -1,1 +1,61 @@
-from sqlalchemy import Column, Integer, String, DateTime
+from sqlalchemy import Column, Integer, String, DateTime, ForeignKey
+from sqlalchemy.orm import relationship
+from database import db, Base
+
+
+class Department(Base):
+    __tablename__ = "departments"
+
+    id = Column(Integer, primary_key=True)
+    department = Column(String(255), nullable= False)
+
+    def __init__(self,  department: str) -> None:
+        self.department = department
+
+    def to_json(self):
+        return {
+            "id": self.id,
+            "department": self.department
+        }
+    
+class Job(Base):
+    __tablename__ = "jobs"
+
+    id = Column(Integer, primary_key= True)
+    job = Column(String(255), nullable= False)
+
+    def __init__(self, job: str) -> None:
+        self.job = job
+
+    def to_json(self):
+        return {
+            "id": self.id,
+            "job": self.job
+        }
+    
+class HiredEmployee(Base):
+    __tablename__ = "hired_employees"
+
+    id = Column(Integer, primary_key= True)
+    name = Column(String(255), nullable= False)
+    datetime = Column(String(255), nullable= False)
+    department_id = Column(ForeignKey("departments.id"))
+    job_id = Column(ForeignKey("jobs.id"))
+
+    departments = relationship("Department", back_populates="hired_employees")
+    jobs = relationship("Job", back_populates="hired_employees")
+
+    def __init__(self, name: str, datetime: str, department_id: int, job_id: int) -> None:
+        self.name = name
+        self.datetime = datetime
+        self.department_id
+        self.job_id
+
+    def to_json(self):
+        return {
+            "id": self.id,
+            "name": self.name,
+            "datetime": self.datetime,
+            "department_id": self.department_id,
+            "job_id": self.job_id
+        }
